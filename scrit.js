6041d0a34466881eeebf67168b494387a3b92add
Alexey Klok <klok.01@list.ru>
2022-05-20 23:17:38 +0300

    second task

--- scrit.js
+++ scrit.js
@@ -7,12 +7,27 @@ const personalMovvieDB = {
     privat: false
 };
 
-const a = prompt('Один из просмотреных фильмов?', ''),
-    b = prompt('Насколько оцениваете его?', ''),
-    c = prompt('Один из просмотреных фильмов?', ''),
-    d = prompt('Насколько оцениваете его?', '');
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из просмотреных фильмов?', ''),
+             b = prompt('Насколько оцениваете его?', '');
 
-personalMovvieDB.movies[a] = b;
-personalMovvieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovvieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('Error');
+        i--;
+    }    
+}
+
+if (personalMovvieDB.count < 10){
+    console.log('sad');
+} else if (personalMovvieDB.count >= 10 && personalMovvieDB.count < 30){
+    console.log('ok');
+} else if (personalMovvieDB.count >= 30){
+    console.log('good');
+} else {
+    console.log('error');
+}
 
 console.log(personalMovvieDB);
